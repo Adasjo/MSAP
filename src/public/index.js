4b88ac95b66b177cf5967bcba4b52f3fcc8f7e9b
Samuel Falk <falkwastaken@gmail.com>
2022-03-25 13:50:23 +0100

    Add structure

--- src/public/index.js
+++ src/public/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
-import './index.css';
+import './styles/index.css';
 import App from './App';
 import counter from './counter';
 
