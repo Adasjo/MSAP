5e9408e6f8b2abb73971a25c769ff7eec558f1ea
Samuel Falk <falkwastaken@gmail.com>
2022-04-08 13:59:17 +0200

    Update spotify handler

--- src/components/spotify.js
+++ src/components/spotify.js
@@ -2,7 +2,7 @@ import React from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { useFirebase } from "react-redux-firebase"
 import { useNavigate } from "react-router-dom"
-import { handleRedirect, spotifyAuthorize } from "../utilities/apiUtils"
+import { handleRedirect } from "../utilities/apiUtils"
 
 function Spotify() {
     const spotify = useSelector(state => state.spotify)
@@ -12,20 +12,13 @@ function Spotify() {
     const navigate = useNavigate()
 
     if (spotify.accessToken || !uid)
-        return null
+        return <div>Waiting for Firebase auth...</div>
 
     if (!spotify.accessToken && window.location.search.length > 10) {
         handleRedirect(dispatch, firebase, navigate).catch(e => console.log(e))
-        return <div>
-                <div>Request processed</div>
-                <div>{JSON.stringify(spotify)}</div>
-            </div>
-    } else if (!spotify.accessToken) {
-        spotifyAuthorize()
-        return <div>Requesting spotify authentification</div>
-    } else {
-        return <div>Already authenticated</div>
+        return <div>Processing Spotify authentification...</div>
     }
+    return <div>Error</div>
 }
 
 export default Spotify
