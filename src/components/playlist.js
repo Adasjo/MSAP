f4198050f2bd92694ba8b8db6252b68fba008254
Samuel Falk <falkwastaken@gmail.com>
2022-04-14 13:51:55 +0200

    Add tracklist component

--- src/components/playlist.js
+++ src/components/playlist.js
@@ -3,6 +3,8 @@ import { useSelector } from "react-redux"
 import { useSearchParams } from "react-router-dom"
 import { spotifyGet } from "../utilities/apiUtils"
 
+import TrackList from "./trackList"
+
 const emptyPlaylist = {
     name: "",
     tracks: {
@@ -17,20 +19,9 @@ function Playlist() {
 
     useEffect(() => spotifyGet("/playlists/" + searchParams.get("id"), token).then(setPlaylist).catch(console.log), [searchParams])
 
-    const trackList = playlist.tracks.items.map(trackRes => trackRes.track).map(track =>
-        <li key={track.id}>
-            <div style={{margin: "30px", minWidth: "30px", minHeight: "30px", display: "flex", justifyContent: "left"}}>
-                <img src={track.album.images[2].url}></img>
-                {track.name}
-                    <audio style={{alignItems: "center", padding: "10px"}} controls name="media">
-                        <source src={track.preview_url} type="audio/mpeg"/>
-                    </audio>
-                </div>
-        </li>);
-
     return <div>
         <h1 style={{margin: "1em"}}>{playlist.name}</h1>
-        <div><ol>{trackList}</ol></div>
+        <TrackList tracks={playlist.tracks.items.map(trackRes => trackRes.track)}/>
     </div>
 }
 
---
+++ src/components/trackList.js
@@ -0,0 +1,34 @@
+import React from "react"
+import "../styles/trackList.css"
+
+function TrackList(props) {
+    const tracks = props.tracks
+
+    function renderArtists(track) {
+        const names = track.artists.map(artist => artist.name)
+        return names.join(", ")
+    }
+
+    function renderTrack(track) {
+        return <li key={track.id}>
+            <div className="track">
+                <div className="nameAndImage">
+                    <img className="trackImage" src={track.album.images[2].url}/>
+                    <span>
+                        <span className="trackName">{track.name}</span><br/>
+                        <span className="trackArtists">{renderArtists(track)}</span>
+                    </span>
+                </div>
+                <audio className="trackPreview" controls name="media">
+                    <source src={track.preview_url} type="audio/mpeg"/>
+                </audio>
+            </div>
+        </li>
+    }
+    
+    return <div>
+        <ol>{tracks.map(renderTrack)}</ol>
+    </div>
+}
+
+export default TrackList
