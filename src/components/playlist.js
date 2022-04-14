a985037712e8bc72c08a8a4195aaddf28f15264d
Samuel Falk <falkwastaken@gmail.com>
2022-04-14 12:04:04 +0200

    Working playlist component

--- src/components/playlist.js
+++ src/components/playlist.js
@@ -3,16 +3,37 @@ import { useSelector } from "react-redux"
 import { useSearchParams } from "react-router-dom"
 import { spotifyGet } from "../utilities/apiUtils"
 
+const emptyPlaylist = {
+    name: "",
+    tracks: {
+        items: []
+    }
+}
+
 function Playlist() {
-    const [playlist, setPlaylist] = useState()
+    const [playlist, setPlaylist] = useState(() => emptyPlaylist)
     const token = useSelector(state => state.spotify.accessToken)
     const [searchParams, _setSearchParams] = useSearchParams()
 
     useEffect(() => spotifyGet("/playlists/" + searchParams.get("id"), token).then(setPlaylist).catch(console.log), [searchParams])
     
-    return <div>
-        Playlist component!
-    </div> 
+    console.log(playlist)
+
+    const tracks = playlist.tracks.items.map(trackRes => trackRes.track).map(track =>
+        <li key={track.id}>
+            <div style={{margin: "30px", minWidth: "30px", minHeight: "30px", display: "flex", justifyContent: "left"}}>
+                <img src={track.album.images[2].url}></img>
+                {track.name}
+                    <audio style={{alignItems: "center", padding: "10px"}} controls name="media">
+                        <source src={track.preview_url} type="audio/mpeg"/>
+                    </audio>
+                </div>
+        </li>);
+
+    return <div style={{ height: "auto", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+        <h1 style={{margin: "1em"}}>{playlist.name}</h1>
+        <div><ol>{tracks}</ol></div>
+    </div>
 }
 
 export default Playlist
