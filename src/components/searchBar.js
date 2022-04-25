13895f07741a82432135d848b6683a8c5046b92c
Samuel Falk <falkwastaken@gmail.com>
2022-04-25 16:32:59 +0200

    Add basic promise feedback to searchbar

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -8,14 +8,14 @@ import TrackList from "./trackList.js"
 import "../styles/search.css"
 import { useSearchParams } from "react-router-dom"
 
-const initRes = {tracks: {items: []}}
 const searchOptions = "&type=track&limit=20"
 const searchInit = new URLSearchParams({
     search: ""
 })
 
 function SearchBar() {
-    const [searchResult, setSearchResult] = useState(() => initRes)
+    const [searchResult, setSearchResult] = useState()
+    const [error, setError] = useState()
     const accesstoken = useSelector(state => state.spotify.accessToken)
     const [searchParams, setSearchParams] = useSearchParams(searchInit)
 
@@ -23,7 +23,7 @@ function SearchBar() {
 
     function search() {
         const searchString = "/search?q=$" + searchText.replace(" ", "%20") + searchOptions
-        spotifyGet(searchString, accesstoken).then(setSearchResult)
+        spotifyGet(searchString, accesstoken).then(setSearchResult).catch(setError)
     }
 
     function updateSearchText(string) {
@@ -32,12 +32,26 @@ function SearchBar() {
     
     useEffect(search, [searchParams])
 
+
+    function promiseNoData() {
+        if (!searchResult) {
+            if (!error) {
+                return <div>Loading...</div>
+            } else {
+                return <div>{error}</div>
+            }
+        }
+        return false
+    }
+
     return <div className="searchComponent">
         <div className="searchHeader">
             <input className="searchBar" type="text" placeholder="Search..." onChange={e => updateSearchText(e.target.value)}/>
             <button className="searchButton" onClick={search}>Search</button>
         </div>
+        {promiseNoData() ||
         <TrackList tracks={searchResult.tracks.items} artistRedirect={updateSearchText} playTrack={track => spotifyPlayTrack(accesstoken, track.uri)}/>
+        }
     </div>
 }
 
