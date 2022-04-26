24965278504185c915a6c0adc2d48769b3d5fc3c
Samuel Falk <falkwastaken@gmail.com>
2022-04-26 13:29:19 +0200

    Add reset password button

--- src/components/signIn.js
+++ src/components/signIn.js
@@ -5,19 +5,38 @@ import { useFirebase } from "react-redux-firebase"
 import "../styles/auth.css" 
 
 function SignIn() {
-    const [email, setEmail] = useState()
-    const [password, setPW] = useState()
+    const [email, setEmail] = useState("")
+    const [password, setPW] = useState("")
     const [error, setError] = useState()
+    const [message, setMessage] = useState()
     const firebase = useFirebase()
     const navigate = useNavigate()
 
     function onSubmit() {
         firebase.login({email, password})
             .then(_ => navigate("/home", {replace: true}))
-            .catch(e => {
-                setError(e)
+            .catch(er => {
+                setError("Incorrect email or password")
                 firebase.initializeAuth()
             })
+    }
+
+    /*
+    *   Send a password reset email to the email.
+    *   The use of `fetchSignInMethodsForEmail` ensures that the user can't see if an email is registered.
+    */
+    async function reset() {
+        setError()        
+        try {
+            const res = await firebase.auth().fetchSignInMethodsForEmail(email)
+
+            // If the length of res is 0, the user is not registered
+            if (res.length == 0 || await firebase.auth().sendPasswordResetEmail(email)) {
+                setMessage("Password reset email sent")
+            }
+        } catch {
+            setError("Invalid email")
+        }
     }
 
     return <div className="authPage">
@@ -30,8 +49,9 @@ function SignIn() {
             <div className="formFieldDiv">
                 <label htmlFor="pw">Password</label>
                 <input className="formField" type="password" onChange={e => setPW(e.target.value)}></input>
-                <Link to="/reset">Forgot password?</Link>
-                {error ? <div className="error">Incorrect email or password</div> : ""}
+                <a onClick={reset} style={{cursor:"pointer"}}>Forgot password?</a>
+                {error ? <div className="error">{error}</div> : ""}
+                {message ? <div>{message}</div> : ""}
             </div>
             <div className="formFieldDiv"><button className="formButton" onClick={onSubmit}>Sign-in</button></div>
             <div className="formFieldDiv">
