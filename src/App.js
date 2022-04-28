e600b8fe17bee978cd5ea0358a3997ec5803cacb
Adam Sjöberg <adasjo@live.se>
2022-04-28 14:56:00 +0200

    signIn refactored

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import './styles/App.css'
 
 import Home from './components/home'
-import SignIn from './components/signIn'
+//import SignIn from './components/signIn'
 //import SignUp from './components/signUp'
 import Spotify from './components/spotify'
 import NoPage from './components/noPage'
@@ -12,6 +12,7 @@ import AuthLock from './components/authLock'
 
 
 const SignUp = require('./components/presenters/signUpPresenter.js').default;
+const SignIn = require('./components/presenters/signInPresenter.js').default;
 
 
 function App() {
---
+++ src/components/presenters/signInPresenter.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import { useFirebase } from "react-redux-firebase"
+import SignInView from "../views/signInView"
+
+import "../../styles/auth.css" 
+
+function SignInPresenter() {
+    const [email, setEmail] = useState("")
+    const [password, setPW] = useState("")
+    const [error, setError] = useState()
+    const [message, setMessage] = useState()
+    const firebase = useFirebase()
+    const navigate = useNavigate()
+
+    function onSubmit() {
+        firebase.login({email, password})
+            .then(_ => navigate("/home", {replace: true}))
+            .catch(er => {
+                setError("Incorrect email or password")
+                firebase.initializeAuth()
+            })
+    }
+
+    /*
+    *   Send a password reset email to the email.
+    *   The use of `fetchSignInMethodsForEmail` ensures that the user can't see if an email is registered.
+    */
+    async function reset() {
+        setError()        
+        try {
+            const res = await firebase.auth().fetchSignInMethodsForEmail(email)
+
+            // If the length of res is 0, the user is not registered
+            if (res.length == 0 || await firebase.auth().sendPasswordResetEmail(email)) {
+                setMessage("Password reset email sent")
+            }
+        } catch {
+            setError("Invalid email")
+        }
+    }
+
+    return <SignInView         
+        email={email}
+        password={password}
+        error = {error}
+        message = {message}
+        setEmail = {setEmail}
+        setPW = {setPW}
+        reset = {reset}
+        onSubmit = {onSubmit}/>
+
+}
+
+
+    
+export default SignInPresenter
