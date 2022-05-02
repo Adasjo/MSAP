4aeaba9856d14f7f5d67c312e8381ca7f116e55e
Adam Sjöberg <adasjo@live.se>
2022-05-03 00:37:23 +0200

    Removed things

--- src/components/presenters/playerPresenter.js
+++ src/components/presenters/playerPresenter.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
-import { initSpotifyPlayerSDK, spotifyPlayTrack, spotifyTransferPlayBack } from "../../utilities/apiUtils"
+import { initSpotifyPlayerSDK, spotifyTransferPlayBack } from "../../utilities/apiUtils"
 
 import PlayerView from "../views/playerView"
 
