43b712cc91ef32a3ea776c685cef0bc0999d8f14
Adam Sjöberg <adasjo@live.se>
2022-05-11 13:02:06 +0200

    Refractored sidebar

---
+++ src/components/presenters/sidebarPresenter.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react"
+import { useSelector } from "react-redux"
+import { useNavigate } from "react-router-dom"
+import { spotifyGet } from "../utilities/apiUtils"
+import "../../styles/sidebar.css"
+import SidebarView from "../views/sidebarView"
+
+function SidebarPresenter() {
+    const accessToken = useSelector(state => state.spotify.accessToken)
+    const spotifyState = useSelector(state => state.spotify.currentState)
+    const [data, setData] = useState()
+
+    useEffect(() => spotifyGet("/me/playlists", accessToken).then(res => setData(res)), [])
+
+    let image = undefined
+
+    if (spotifyState) {
+        image = spotifyState.track_window.current_track.album.images[0].url
+    }
+
+    function renderPlaylists() {
+        return data.items.map(playlist => 
+            <div key={playlist.id}>
+                <button className="playlistButton" onClick={() => useNavigate("/home/playlist?id=" + playlist.id)}>{playlist.name}</button>
+            </div>
+        )
+    } 
+
+    return <SidebarView
+    renderPlaylists={renderPlaylists}
+    data={data}
+    image={image}    />
+}
+
+export default SidebarPresenter
--- src/components/views/homeView.js
+++ src/components/views/homeView.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Routes, Route, useNavigate } from 'react-router-dom'
 import Playlist from "../playlist"
 import Player from "../presenters/playerPresenter"
-import Sidebar from "../Sidebar"
+import Sidebar from "../presenters/sidebarPresenter"
 import Queue from '../presenters/queuePresenter'
 import SearchbarPresenter from '../presenters/searchbarPresenter'
 import ProfilePresenter from "../presenters/profilePresenter"
