9688bc6623b845162ff89558afa7efb43d63f629
Adam Sjöberg <adasjo@live.se>
2022-05-12 15:06:23 +0200

    Infinity scroll playlisttracks

--- src/components/views/playlistView.js
+++ src/components/views/playlistView.js
@@ -2,7 +2,7 @@ import React from "react"
 
 
 function PlaylistView({playlistName, trackList}){
-    return <div className="playlist">
+    return <div className="playlist" id="scrollableDiv">
         <h1 className="playlistHeader">{playlistName}</h1>
         {trackList}
     </div> 
