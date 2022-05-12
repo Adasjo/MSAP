dc691093e03e7d55ce31b8c63fdb8890e42aec1d
Samuel Falk <falkwastaken@gmail.com>
2022-05-12 13:44:11 +0200

    Fix stuff
    Co-authored-by: Adasjo <adasjo@kth.se>

--- src/components/presenters/searchbarPresenter.js
+++ src/components/presenters/searchbarPresenter.js
@@ -4,8 +4,6 @@ import { useSelector } from "react-redux"
 import { useSearchParams } from "react-router-dom"
 import { spotifyGet, spotifyPlayTrack, spotifyQueueTrack } from '../../utilities/apiUtils'
 import { debounce } from "../../utilities/utils"
-import ArtistList from "../views/artistList"
-
 
 import "../../styles/search.css"
 
@@ -15,9 +13,8 @@ import TrackListView from "../views/trackListView"
 
 import spinner from "../../assets/spinner.gif"
 
-let offset = 0;
-const limit = 20;
-let searchOptions = "&type=track&limit=" + limit.toString() + "&offset=" + offset.toString();
+const limit = 40
+const searchOptions = "&type=track&limit=" + limit
 const searchInit = new URLSearchParams({
     search: ""
 })
@@ -30,14 +27,17 @@ function SearchbarPresenter() {
     const [searchParams, setSearchParams] = useSearchParams(searchInit)
     const [loading, setLoading] = useState()
     const [tracks, setTracks] = useState()
+    const [hasMore, setHasMore] = useState(true)
+    const [offset, setOffset] = useState(0)
 
     let searchText = searchParams.get("search")
 
     async function search() {
-        const searchString = "/search?q=$" + searchText.replace(" ", "%20") + searchOptions
+        const searchString = "/search?q=$" + searchText.replace(" ", "%20") + "&offset=0" + searchOptions
         setLoading(true)
         spotifyGet(searchString, accessToken).then(res => {
-            setTracks(res.tracks.items);
+            setOffset(0)
+            setTracks([...res.tracks.items]);
             setSearchResult(res);
             setLoading(false)
         }).catch(setError)
@@ -48,7 +48,6 @@ function SearchbarPresenter() {
 
     function updateSearchText(string) {
         setSearchDebounced(new URLSearchParams({search: string}))
-        setTracks([]);
     }
     
     function artistRedirect(e, artist) {
@@ -57,15 +56,16 @@ function SearchbarPresenter() {
     }
 
     useEffect(search, [searchParams])
-    const handleScroll = () => {
-        offset = offset + limit;
-        searchOptions = "&type=track&limit=" + limit.toString() + "&offset=" + offset.toString();
-        const searchString = "/search?q=$" + searchText.replace(" ", "%20") + searchOptions
+
+    function handleScroll() {
+        const newOffset = offset + limit
+        const searchString = "/search?q=$" + searchText.replace(" ", "%20") + "&offset=" + newOffset + searchOptions
         setLoading(true)
+        setOffset(newOffset)
         spotifyGet(searchString, accessToken).then(res => {
-            let tempTracks = tracks;
-            res.tracks.items.forEach(e => tempTracks.push(e));
-            setTracks(tempTracks)
+            const newTracks = [...tracks, ...res.tracks.items]
+            setHasMore(res.tracks.items.length > 0)
+            setTracks([...newTracks])
             setLoading(false)
         }).catch(setError)
     }
@@ -78,7 +78,7 @@ function SearchbarPresenter() {
         }
         return false
     }
-    
+
     return <SearchbarView id="scrollableDiv"
         updateSearchText = {updateSearchText}
         loading = {loading}
@@ -87,7 +87,7 @@ function SearchbarPresenter() {
             <InfiniteScroll 
                 dataLength={tracks.length}
                 next={handleScroll}
-                hasMore={true}
+                hasMore={hasMore}
                 loader={<h4>Loading...</h4>}
                 endMessage={
                     <p style={{ textAlign: 'center' }}>
